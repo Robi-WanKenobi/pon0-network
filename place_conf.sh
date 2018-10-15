ba315d65529f69674081f12cbdef9cdf87da3015
Ben Kenobi <roberto.arranz.93@gmail.com>
2018-10-15 18:46:40 +0200

    Update place_conf.sh

--- place_conf.sh
+++ place_conf.sh
@@ -12,8 +12,7 @@ set -u
 
 TMP_CONF='/pon0.conf.tmp'
 TMP_CONF2='/pon1.conf.tmp'
-TMP_CONF3='/pon3.conf.tmp'
-TMP_CONF4='/pon4.conf.tmp'
+TMP_CONF02='/pon0-2.conf.tmp'
 
 # If specified, overwrite the network configuration file.
 if [ "${CNI_NETWORK_CONFIG:-}" != "" ]; then
@@ -28,17 +27,12 @@ ${CNI_PON1_NETWORK_CONFIG:-}
 EOF
 fi
 
-if [ "${CNI_PON3_NETWORK_CONFIG:-}" != "" ]; then
-cat >$TMP_CONF3 <<EOF
-${CNI_PON3_NETWORK_CONFIG:-}
+if [ "${CNI_PON02_NETWORK_CONFIG:-}" != "" ]; then
+cat >$TMP_CONF02 <<EOF
+${CNI_PON02_NETWORK_CONFIG:-}
 EOF
 fi
 
-if [ "${CNI_PON4_NETWORK_CONFIG:-}" != "" ]; then
-cat >$TMP_CONF4 <<EOF
-${CNI_PON4_NETWORK_CONFIG:-}
-EOF
-fi
 
 # Move the temporary CNI config into place.
 FILENAME=${CNI_CONF_NAME:-20-pon0.conf}
@@ -49,13 +43,9 @@ FILENAME2=${CNI_CONF_NAME2:-25-pon1.conf}
 mv $TMP_CONF2 /host/etc/cni/net.d/${FILENAME2}
 echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${FILENAME2})"
 
-FILENAME3=${CNI_CONF_NAME3:-30-pon3.conf}
-mv $TMP_CONF3 /host/etc/cni/net.d/${FILENAME3}
-echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${FILENAME3})"
-
-FILENAME4=${CNI_CONF_NAME4:-35-pon4.conf}
-mv $TMP_CONF4 /host/etc/cni/net.d/${FILENAME4}
-echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${FILENAME4})"
+FILENAME02=${CNI_CONF_NAME3:-22-pon0-2.conf}
+mv $TMP_CONF02 /host/etc/cni/net.d/${FILENAME02}
+echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${FILENAME02})"
 
 # Unless told otherwise, sleep forever.
 # This prevents Kubernetes from restarting the pod repeatedly.
